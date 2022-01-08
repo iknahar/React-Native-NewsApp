cc08509ce8ec0248d7b90412686006a5b52b3ac9
Kamrun Nahar <knahar.kamrun@gmail.com>
2022-01-08 19:50:04 +0600

    details page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, Image } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons'; 
+import { StyleSheet, Text, View, Platform, Image, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Cards = props => {
-   
+
     return (
-        <View style={styles.card}>
-            <View style={styles.imageContainer}>
-                {/* <Image style={styles.image} source={ require('../../assets/ScreenShot_20220108152310.png') }/> */}
-                <Image style={styles.image} source={{ uri: 'https://www.who.int/images/default-source/mca/mca-covid-19/coronavirus-2.tmb-1024v.jpg?Culture=en&sfvrsn=4dba955c_6' } }/>
+        <TouchableOpacity onPress={() => {
+                props.navigation.navigate('NewsItem')
+            }
+        }>
+            <View style={styles.card}>
+                <View style={styles.imageContainer}>
+                    {/* <Image style={styles.image} source={ require('../../assets/ScreenShot_20220108152310.png') }/> */}
+                    <Image style={styles.image} source={{ uri: 'https://www.who.int/images/default-source/mca/mca-covid-19/coronavirus-2.tmb-1024v.jpg?Culture=en&sfvrsn=4dba955c_6' }} />
+                </View>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Covid cases top 1.17 lakh; infection spreads among doctors</Text>
+                    <MaterialIcons name="favorite-border" size={24} color="#72bcd4" />
+                </View>
+                <View style={styles.description}>
+                    <Text style={styles.descriptionText}>India revised the home quarantine rules for international arrivals as the country’s Covid-19 count surged to more than 1.17 lakh infections on Friday. </Text>
+                </View>
             </View>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>Covid cases top 1.17 lakh; infection spreads among doctors</Text>
-                <MaterialIcons name="favorite-border" size={24} color="#72bcd4" />
-            </View>
-            <View style={styles.description}>
-                <Text style={styles.descriptionText}>India revised the home quarantine rules for international arrivals as the country’s Covid-19 count surged to more than 1.17 lakh infections on Friday. </Text>
-            </View>
-        </View>
+        </TouchableOpacity>
     );
 };
 
@@ -44,7 +49,7 @@ const styles = StyleSheet.create({
         height: '100%',
         width: '100%',
         // resizeMode: 'contain',
-        resizeMode: 'cover',      
+        resizeMode: 'cover',
 
 
     },
@@ -54,22 +59,22 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent:'space-between'
+        justifyContent: 'space-between'
         // justifyContent:'center'
     },
     title: {
         fontFamily: 'newsreader-semibold',
-        fontWeight:'bold',
+        fontWeight: 'bold',
         marginTop: 4,
-        fontSize:16,
+        fontSize: 16,
     },
     description: {
-        paddingHorizontal:20
+        paddingHorizontal: 20
     },
     descriptionText: {
         fontFamily: 'newsreader-light',
         marginTop: 5,
-        fontSize:14,
+        fontSize: 14,
     }
 
 });
--- src/screens/NewsListScreen.js
+++ src/screens/NewsListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Platform, Image } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import Cards from '../components/Cards';
 
@@ -8,12 +8,7 @@ const NewsListScreen = props => {
     console.log(props);
     return (
         <View>
-            <TouchableOpacity onPress={() => {
-                props.navigation.navigate('NewsItem')
-            }}>
-                <Cards></Cards>
-            </TouchableOpacity>
-
+            <Cards navigation={props.navigation} />
         </View>
     );
 };
